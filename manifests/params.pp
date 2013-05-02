0dc316a80569478e3a20a465c0d080e4cdf236b6
Mingxiang.Zhang <ming.zhang@gree.net>
2013-05-02 11:46:46 +0900

    enable multiple instances in one host, tested only at debian

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,6 +6,10 @@ class memcached::params {
       $dev_package_name = 'libmemcached-dev'
       $config_file      = '/etc/memcached.conf'
       $config_tmpl      = "$module_name/memcached.conf.erb"
+      $init_file        = "/etc/init.d/memcached"
+      $init_tmpl        = "$module_name/memcached_init_debian.erb"
+      $default_file     = "/etc/default/memcached"
+      $default_src      = "$module_name/memcached_default"
       $user             = 'nobody'
     }
     'RedHat': {
@@ -14,6 +18,8 @@ class memcached::params {
       $dev_package_name = 'libmemcached-devel'
       $config_file      = '/etc/sysconfig/memcached'
       $config_tmpl      = "$module_name/memcached_sysconfig.erb"
+      $init_file        = "/etc/init.d/memcached"
+      $init_tmpl        = "$module_name/memcached_init.erb"
       $user             = 'memcached'
     }
     default: {
